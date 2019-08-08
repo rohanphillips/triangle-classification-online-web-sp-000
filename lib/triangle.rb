f42be8936a16ecc88f5ddfdf05410d58ce16ae95
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-08 19:43:48 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+require "pry"
 class Triangle
-  # write code here
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    valid_sides = @side1 > 0 && @side2 > 0 && @side3 > 0
+    inequality = (@side1 + @side2 > @side3) && (@side2 + @side3 > @side1) && (@side1 + @side3 > @side2)
+    equilateral = @side1 == @side2 && @side1 == @side3
+    isosceles = (@side1 == @side3 && @side2 != @side1) || (@side2 == @side3 && @side1 != @side2) || (@side1 == @side2 && @side1 != @side3)
+    scalene = @side1 != @side2 && @side2 != @side3
+
+    if valid_sides == false || (inequality == false)
+      raise TriangleError
+    end
+    if equilateral
+      return :equilateral
+    elsif isosceles
+      return :isosceles
+    elsif scalene
+      return :scalene
+    end
+
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+    def message
+      "This triangle sucks"
+    end
+  end
 end
