7274a17b178fb4be64b2f2adf0cd45a8b5ea4b2c
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-08 19:43:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
 
   def kind
     valid_sides = @side1 > 0 && @side2 > 0 && @side3 > 0
-    inequality = (@side1 + @side2 > @side3) || (@side2 + @side3 > @side1) || (@side1 + @side3 > @side2)
+    inequality = (@side1 + @side2 > @side3) && (@side2 + @side3 > @side1) && (@side1 + @side3 > @side2)
     equilateral = @side1 == @side2 && @side1 == @side3
     isosceles = (@side1 == @side3 && @side2 != @side1) || (@side2 == @side3 && @side1 != @side2) || (@side1 == @side2 && @side1 != @side3)
     scalene = @side1 != @side2 && @side2 != @side3
